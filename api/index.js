d38babaf99b6b8b8ea9e3cc575535fa5046f1485
Deimantas Špučys <as.deimis@gmail.com>
2020-02-05 23:35:01 -0800

    express serverless fn chore

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ app.get('/api', function(req, res) {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/.netlify/functions/server', router);
+app.use('/.netlify/functions/start', router);
 
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
 
